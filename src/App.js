cb86b08e16d46ea2112b51f664f2ce65baf8088b
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-04 17:19:06 +0530

    very important by vikram by direct call to function

--- src/App.js
+++ src/App.js
@@ -10,33 +10,16 @@ export default function App() {
 }
 
 function MyComponent() {
-  let [list, setList] = useState(["Hello Worldd"]);
-
-  const tweetHere = () => {
-    const newlist = [...list, "Hello universe"];
-    setList(newlist);
-  };
-
-  const deleteFirstTweet = () => {
-    list.splice(0, 1);
-    const newlist = [...list];
-    setList(newlist);
-  };
+  let [counter, setCounter] = useState(0);
 
   return (
     <div>
-      <h1>Working with Input Element</h1>
-      <input type="button" value="Tweet to Everyone" onClick={tweetHere} />
+      <div className="fs-2">Like {counter}</div>
       <input
         type="button"
-        value="Delete First Tweet"
-        onClick={deleteFirstTweet}
+        value="Like Me.."
+        onClick={() => setCounter(counter++)}
       />
-      <div>
-        {list.map((item) => (
-          <div>{item}</div>
-        ))}
-      </div>
     </div>
   );
 }
