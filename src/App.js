0f79e3a7db596f787fe3bad0d9b466abb416e15b
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-04 13:09:24 +0530

    app.js modified with we can use interpoiation in attribute

--- src/App.js
+++ src/App.js
@@ -3,32 +3,22 @@ export default function App() {
     <div>
       <div>Hello</div>
       <MyComponent />
+      <MyComponent></MyComponent>
     </div>
   );
 }
 
 function MyComponent() {
-  let id = 1;
-  let username = `Vrushabh`;
-  let email = `vrushabh@gmail.com`;
-  let list = ["delhi", "Pune", "Amravti"];
-  let obj = { id: "1", username: "DhoneVrushabh" };
+  let str1 = `Hello Whats going on`;
+  let str2 = `Whats up dude ,How are ypu`;
+  let str3 = `I am Fine what about you`;
+  let att1 = "bg-info p-3 fs-1 mb-1";
+  let att2 = "bg-danger p-3 fs-1 mb-1";
   return (
     <div>
-      <h1>Learning Interpolation</h1>
-      <h1>
-        {username} {id} {email} {list}
-      </h1>
-      <h1>{`i am also js ${obj.id} ${obj.username}`}</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        perspiciatis autem eligendi sapiente doloribus quidem ratione ab tempora
-        corporis, est ipsam id eos amet molestiae, accusamus voluptas numquam ut
-        nobis vitae optio! Hic quidem, doloremque laborum recusandae autem ad
-        accusantium aspernatur nisi. Dolorum facere fugit placeat ipsum eius
-        tempora ipsam.
-      </p>
+      <div className="bg-danger p-3 fs-1 mb-1">{str1}</div>
+      <div className={att1}>{str2.toUpperCase()}</div>
+      <div className={att2}> {str3.toLocaleLowerCase()}</div>
     </div>
   );
 }
