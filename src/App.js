775cdc17cccced418acae8aa6a84e682f73bfcb8
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-04 14:49:13 +0530

    app.js modified like application

--- src/App.js
+++ src/App.js
@@ -9,44 +9,35 @@ export default function App() {
 }
 
 function MyComponent() {
-  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const [headerCss, setheaderCss] = useState(
-    "bg-dark text-light p-2 fs-1 fw-bold"
-  );
-  const [paraCss, setparaCss] = useState(
-    "alert alert-danger text-dark fw-bold font-monospace"
-  );
-  let tweet = `      let str1=Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-      iure non placeat voluptatem aut mollitia doloribus aspernatur? Harum
-      quibusdam nobis, eveniet sunt corrupti rerum reiciendis dolorem omnis quod
-      optio aliquid architecto consequuntur minus suscipit. Eos, assumenda
-      dolorum excepturi neque quod, aliquid autem reprehenderit tenetur
-      provident molestias minus non ea. Veritatis.`;
-
-  const customCSS1 = () => setparaCss("bg-info text-light p-2 fs-3 fw-bold");
-  const customCSS2 = () => setparaCss("bg-warning text-light p-2 fs-3 fw-bold");
-  const customCSS3 = () => setparaCss("bg-success text-light p-2 fs-3 fw-bold");
-
+  let [counter, setCounter] = useState(0);
+  const increment = () => {
+    counter++;
+    setCounter(counter);
+  };
   return (
     <div>
-      <input type="button" value="button1" onClick={customCSS1} />
-      <input type="button" value="button2" onClick={customCSS2} />
-      <input type="button" value="button3" onClick={customCSS3} />
-      <div>
-        {list.map(() => (
-          <div>
-            <div
-              className={headerCss}
-              style={{ borderBottom: "red solid 5px" }}
-            >
-              Hello
-            </div>
-            <div className={paraCss}>
-              {tweet} {paraCss}
-            </div>
-          </div>
-        ))}
-      </div>
+      <h1>Like Application</h1>
+      <input
+        className="btn btn-outline-danger my-1"
+        type="button"
+        value="Like Me."
+        onClick={increment}
+      />
+
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
+      <div>Like {counter}</div>
     </div>
   );
 }
