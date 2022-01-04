64bebf3d1d9424cb84cdd5dafea64661ae510bcb
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-04 12:37:43 +0530

    app.js modified with interpolation and component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Hello</div>
+      <MyComponent />
     </div>
   );
 }
 
-export default App;
+function MyComponent() {
+  let styleCSS = "bg-black text-light p-2 fs-1";
+  let styleCSS1 = "bg-danger text-light p-2 fs-1 mt-1";
+  return (
+    <div>
+      <div className={styleCSS}>Hello Vrushabh</div>
+      <div className={styleCSS1}>This is my component</div>
+    </div>
+  );
+}
